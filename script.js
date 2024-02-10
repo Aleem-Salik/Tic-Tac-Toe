d5fe8a5cd995e73b49ac91cbe8022116bcb7d6d6
Aleem-Salik <aleemsalik09@gmail.com>
2024-02-10 14:23:39 +0530

    add winner checking

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 const container = document.querySelector(".container");
 const cells = document.querySelectorAll(".cell");
-const circle =
-  "<img data-player='circle' class='circle' src='images/circle.svg' alt='circle' />";
-const cross =
-  "<img data-player='cross' class='cross' src='images/cross.svg' alt='cross' />";
-let move = cross;
+const heading = document.querySelector(".heading");
+const players = {
+  cross:
+    "<img data-player='cross' class='cross' src='images/cross.svg' alt='cross' />",
+  nought:
+    "<img data-player='circle' class='nought' src='images/circle.svg' alt='circle' />",
+};
+
+let move = "cross";
+
+const checkWinner = function (line) {
+  const rowData = [];
+  document.querySelectorAll(`.${line}`).forEach((el) => {
+    rowData.push(el.firstElementChild?.dataset.player);
+  });
+  if (rowData[0] == rowData[1] && rowData[1] == rowData[2]) {
+    heading.innerText = `${move.toUpperCase()} WINS 🏆`;
+    document.querySelectorAll(`.${line}`).forEach((cell) => {
+      cell.style.backgroundColor = "Seagreen";
+    });
+  }
+};
+
+const checkDiagonal = function (element, index) {
+  if (element.classList[index]) {
+    const diagonal = element.classList[index];
+    checkWinner(diagonal);
+  }
+};
 
 container.addEventListener("click", function (e) {
-  if (!e.target) return;
-  if (!e.target.classList.contains("cell")) return;
-  e.target.innerHTML = move;
+  const element = e.target;
+  if (element.innerHTML) return;
+  if (!element) return;
+  if (!element.classList.contains("cell")) return;
+  element.innerHTML = players[move];
 
-  move = move === cross ? circle : cross;
+  const row = element.classList[1];
+  checkWinner(row);
+
+  const column = element.classList[2];
+  checkWinner(column);
+
+  checkDiagonal(element, 3);
+  checkDiagonal(element, 4);
+
+  move = move === "cross" ? "nought" : "cross";
 });
